6e1934ed6fbda5a921b6e93319ee0b3a0b765866
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-06-06 15:59:05 +0300

    expanding one item at a time

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,48 +1,49 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import LionessContext from '../../LionessContext/LionessContext';
 import './Project.css'
-export default class Project extends Component{
-    static contextType= LionessContext
-    constructor(){
+export default class Project extends Component {
+    static contextType = LionessContext
+    constructor() {
         super()
-        this.state= {
-            on: false,
+        this.state = {
+            expandedIndex: false,
         }
     }
-    toggle=()=>{
+    toggle = (index) => {
+        const { expandedIndex } = this.state
         this.setState({
-            on: !this.state.on
+            expandedIndex: expandedIndex === index ? -1 : index
         })
     }
-render(){
-    
-    const projects= this.context.projects.map(project=>{
-        
-        const projectDetails= 
-        <section key={project.id}>
-            <button className='collapsible'onClick={this.toggle}>{project.title}
-            <em>${project.budget}</em></button>
-{this.state.on &&
- <div className='project-content'>
-     <p><em>Title:</em>{project.title}</p>
-     <p><em>Client:</em>{project.client[0].full_name}</p>
-     <p><em>Project Manager:</em>{project.projectManager[0].full_name}</p>
-     <p><em>Description :</em>{project.description}</p>
-     <p><em>Budget:</em>{project.budget}</p>
-     <p><em>Status:</em>{project.status.title}</p>
-     <p><em>Start Date:</em>{project.startDate}</p>
-     {project.estimatedDueDate ? <p><em>Estimated Due Date:</em>{project.estimatedDueDate}</p> : ""}
-     {project.completionDate ?  <p><em>Completion Date:</em>{project.completionDate}</p> : ''}
-</div>
-}
-        </section>
-        return projectDetails
-    })
-   
-    return(
-        <div className='tab-content'>
-            {projects}
+    render() {
+
+        const projects = this.context.projects.map((project, index) => {
+            const expandedClassName = this.state.expandedIndex === index ? 'expanded' : ''
+            const projectDetails =
+                <section key={project.id}>
+                    <button className='collapsible' onClick={() => { this.toggle(index) }}>{project.title}
+                        <em>${project.budget}</em></button>
+                    
+                        <div className={`project-content ${expandedClassName}`}>
+                            <p><em>Title:</em>{project.title}</p>
+                            <p><em>Client:</em>{project.client[0].full_name}</p>
+                            <p><em>Project Manager:</em>{project.projectManager[0].full_name}</p>
+                            <p><em>Description :</em>{project.description}</p>
+                            <p><em>Budget:</em>{project.budget}</p>
+                            <p><em>Status:</em>{project.status.title}</p>
+                            <p><em>Start Date:</em>{project.startDate}</p>
+                            {project.estimatedDueDate ? <p><em>Estimated Due Date:</em>{project.estimatedDueDate}</p> : ""}
+                            {project.completionDate ? <p><em>Completion Date:</em>{project.completionDate}</p> : ''}
+                        </div>
+                    
+                </section>
+            return projectDetails
+        })
+
+        return (
+            <div className='tab-content'>
+                {projects}
             </div>
-    )
-}
+        )
+    }
 }
