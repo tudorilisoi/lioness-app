1d356273b15abc7b9cc05369bdd570c6024bd48a
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-06-13 17:27:47 +0300

    adding client

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,57 +1,87 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import LionessContext from '../../LionessContext/LionessContext';
 import EditUser from '../EditUser/EditUser'
-export default class Client extends Component{
-static contextType= LionessContext
-constructor(){
-    super()
-    this.state = {
-        expandedIndex: false,
-        editModeIndex: false,
+
+const newClientTemplate = {
+    id: -1,
+    email: '',
+    username: '',
+    full_name: '',
+    phone: '',
+    password: '',
+    //associated objects
+    role: null, //TODO add a role object
+    // isAdmin: true,
+    projects: []
+};
+
+
+export default class Client extends Component {
+    static contextType = LionessContext
+    constructor() {
+        super()
+        this.state = {
+            newClient: null,
+            expandedIndex: false,
+            editModeIndex: false,
+        }
     }
-}
-toggle = (index) => {
-    const { expandedIndex } = this.state
-    this.setState({
-        expandedIndex: expandedIndex === index ? -1 : index
-    })
-}
-toggleeditModeIndex = (index) => {
-    const { editModeIndex } = this.state
-    this.setState({
-        editModeIndex: editModeIndex === index ? -1 : index
-    })
-}
-render(){  
-    console.log(this.state)
-    // debugger
-    const clients= this.context.users.data.map((user, index)=>{
-        const expandedClassName = this.state.expandedIndex === index ? 'expanded' : ''
-        const isEditing = this.state.editModeIndex === index
-        const editingModeClassName=isEditing ? 'show':''
-        const clientDetails= 
-        <section key={user.id}>
-            <button className='collapsible' onClick={()=> {this.toggle(index)}}>{user.full_name}
-            
-           </button>
-          
-        
- <div className={`button-content ${expandedClassName}`}>
-   
-  <button>Delete</button>
+    toggle = (index) => {
+        const { expandedIndex } = this.state
+        this.setState({
+            expandedIndex: expandedIndex === index ? -1 : index
+        })
+    }
+    toggleeditModeIndex = (index) => {
+        const { editModeIndex } = this.state
+        this.setState({
+            editModeIndex: editModeIndex === index ? -1 : index
+        })
+    }
+
+    addClient = () => {
+        this.setState({
+            editModeIndex: 0,
+            expandedIndex: 0,
+            newClient: { ...newClientTemplate }
+        })
+    }
+
+
+    render() {
+        console.log(this.state)
+        // debugger
+        const clientsList = [...this.context.users.data]
+        if (this.state.newClient) {
+            clientsList.unshift(this.state.newClient)
+        }
+        const clients = clientsList.map((user, index) => {
+            const expandedClassName = this.state.expandedIndex === index ? 'expanded' : ''
+            const isEditing = this.state.editModeIndex === index
+            const editingModeClassName = isEditing ? 'show' : ''
+            const clientDetails =
+                <section key={user.id}>
+                    <button className='collapsible' onClick={() => { this.toggle(index) }}>{user.full_name}
+
+                    </button>
+
+
+                    <div className={`button-content ${expandedClassName}`}>
+
+                        <button>Delete</button>
                         <button onClick={() => this.toggleeditModeIndex(index)}>Edit</button>
-                        <EditUser user={user} editMode={isEditing} role={this.props.role}/>
+                        <EditUser user={user} editMode={isEditing} role={this.props.role} />
                         <button className={`saveButton ${editingModeClassName}`}>Save</button>
 
-</div>
+                    </div>
 
-        </section>
-        return clientDetails
-    })
-        return(
+                </section>
+            return clientDetails
+        })
+        return (
             <div className='tab-content'>
-              {clients}
-                </div>
+                {clients}
+            </div>
         )
     }
 }
--- src/components/Client/ClientPage.js
+++ src/components/Client/ClientPage.js
@@ -1,27 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import DataLoader from '../DataLoader/DataLoader'
 import LionessContext from '../../LionessContext/LionessContext'
 import ds from '../../STORE/dataservice';
 import Client from '../Client/Client'
 import Navbar from '../Nav/Nav'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import UserSearchBar from '../UserSearchBar/UserSearchBar'
-const {getUsers, getProjects, handleFetchError }  =ds
-export default class ClientPage extends Component{
-
-static contextType= LionessContext;
-    render(){
-        const opts= {roleFilter: 2}
-        return(
+const { getUsers, getProjects, handleFetchError } = ds
+export default class ClientPage extends Component {
+    constructor(props) {
+        super(props)
+        this.clientRef = null;
+    }
+    static contextType = LionessContext;
+    render() {
+        const opts = { roleFilter: 2 }
+        return (
             <div className='tab-page'>
                 <h2>Clients</h2>
-                <UserSearchBar role={opts.roleFilter}/>
-                <Link to='/add-client-form'>
-                <button>Add Client</button>
-                </Link>
+                <UserSearchBar role={opts.roleFilter} />
+                {/* <Link to='/add-client-form'>
+                </Link> */}
+                <button onClick={ev=>{this.clientRef && this.clientRef.addClient()}}>Add Client</button>
                 <button>Email Client</button>
-                
-                 <Client role={opts.roleFilter}/>
+                <Client ref={ref => this.clientRef = ref} role={opts.roleFilter} />
             </div>
         )
     }
