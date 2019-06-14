5de486bd7f90f796368e2b88e1c5d5f5cd8e1b6e
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-06-14 12:37:28 +0300

    CSS

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -90,7 +90,11 @@ export default class Project extends Component {
                                 {!this.state.newProject ? <button className='actionButton'><Icon icon="trash" /> Delete</button> : ''}
                                 {!this.state.newProject ?
                                     <button className='actionButton' onClick={() => this.toggleeditModeIndex(index)}><Icon icon="edit" /> Edit</button> : ''}
-                                    <button><Icon icon='fa-close'/></button>
+                                <a
+                                    onClick={() => { this.toggle(index) }}
+                                    href="#" className='closeButton'>
+                                    <Icon icon='times' />
+                                </a>
                             </div>
                             <EditProject project={project} editMode={isEditing} />
                             <div>
