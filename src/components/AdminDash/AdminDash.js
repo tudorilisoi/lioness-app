aba32e136fa57792168f3b3b8b7e8ccd389dc84c
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-06-14 10:48:41 +0300

    CSS/UI

--- src/components/AdminDash/AdminDash.js
+++ src/components/AdminDash/AdminDash.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import { NavTab} from "react-router-tabs";
+import React, { Component } from 'react'
+import { NavTab } from "react-router-tabs";
 import EstimatesPage from '../Projects/EstimatesPage'
 import ClientPage from '../Users/ClientPage'
 import ActiveProjectsPage from '../Projects/ActiveProjectsPage'
@@ -7,59 +7,61 @@ import BilledProjectsPage from '../Projects/BilledProjectsPage'
 import ProjectManagerPage from '../Users/ProjectManagerPage'
 import ContractorsPage from '../Users/ContractorsPage'
 import HomePage from '../Home/HomePage'
-import { Route, Switch, Redirect} from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import ds from '../../STORE/dataservice';
 import './AdminDash.css'
 import LionessContext from '../../LionessContext/LionessContext'
 
 import "react-tabs/style/react-tabs.css";
-const {deleteCookieLoginInfo} = ds
-export default class AdminDash extends Component{
-static contextType= LionessContext
-logoutClick=()=>{
-    deleteCookieLoginInfo()
-    this.props.history.push('/login')
-}
-    render(){  
-        const {path}=this.props.match   
-        return(
+const { deleteCookieLoginInfo } = ds
+export default class AdminDash extends Component {
+    static contextType = LionessContext
+    logoutClick = () => {
+        deleteCookieLoginInfo()
+        this.props.history.push('/login')
+    }
+    render() {
+        const { path } = this.props.match
+        return (
             <div>
-            <nav role='navigation' className="navBar">
-                <div className='heading'>
-            <h3>Lioness</h3>
-         <h4>Welcome!</h4>
-         <button onClick={()=>this.logoutClick()}>
-            Log Out
-        </button>
-        </div>
-                <div className='links'>
-                
-                    <NavTab to={`${path}/home`}className='link'>Home</NavTab>
-                    <NavTab to={`${path}/estimates`}className='link'>Estimates</NavTab>
-                    <NavTab to={`${path}/active-projects`}className='link'>Active Projects</NavTab>
-                <NavTab to={`${path}/billed-projects`}className='link'>Billed Projects</NavTab>
-                <NavTab to={`${path}/clients`}className='link'>Clients</NavTab>
-                    <NavTab to={`${path}/project-managers`}className='link'>Project Managers</NavTab>
-                    <NavTab to={`${path}/contractors`}className='link'>Contractors</NavTab>
-                </div>
-         <div className='tabs'>
-             <Switch>
-             <Route
-          exact
-          path={`${path}`}
-          render={() => <Redirect replace to={`${path}/home`} />}
-        />
-             <Route path={`${path}/home`} exact component={HomePage}></Route>
-             <Route path={`${path}/clients`}  exact component={ClientPage}></Route>
-             <Route path={`${path}/estimates`}  exact component={EstimatesPage}></Route>
-             <Route path={`${path}/active-projects`}  exact component={ActiveProjectsPage}></Route>
-             <Route path={`${path}/billed-projects`}  exact component={BilledProjectsPage}></Route>
-             <Route path={`${path}/project-managers`}  exact component={ProjectManagerPage}></Route>
-             <Route path={`${path}/contractors`}  exact component={ContractorsPage}></Route>
-             </Switch>
-             
-         </div>
-        </nav>
+                <nav role='navigation' className="navBar">
+                    <div className='heading'>
+                        <div className='logo flexed'>
+                            <h3>Lioness</h3>
+                        </div>
+                        <div className='userInfo flexed'>
+                            <h4>Welcome!</h4>
+                            <button onClick={() => this.logoutClick()}>Log Out</button>
+                        </div>
+                    </div>
+                    <div className='links'>
+
+                        <NavTab to={`${path}/home`} className='link'>Home</NavTab>
+                        <NavTab to={`${path}/estimates`} className='link'>Estimates</NavTab>
+                        <NavTab to={`${path}/active-projects`} className='link'>Active Projects</NavTab>
+                        <NavTab to={`${path}/billed-projects`} className='link'>Billed Projects</NavTab>
+                        <NavTab to={`${path}/clients`} className='link'>Clients</NavTab>
+                        <NavTab to={`${path}/project-managers`} className='link'>Project Managers</NavTab>
+                        <NavTab to={`${path}/contractors`} className='link'>Contractors</NavTab>
+                    </div>
+                    <div className='tabs'>
+                        <Switch>
+                            <Route
+                                exact
+                                path={`${path}`}
+                                render={() => <Redirect replace to={`${path}/home`} />}
+                            />
+                            <Route path={`${path}/home`} exact component={HomePage}></Route>
+                            <Route path={`${path}/clients`} exact component={ClientPage}></Route>
+                            <Route path={`${path}/estimates`} exact component={EstimatesPage}></Route>
+                            <Route path={`${path}/active-projects`} exact component={ActiveProjectsPage}></Route>
+                            <Route path={`${path}/billed-projects`} exact component={BilledProjectsPage}></Route>
+                            <Route path={`${path}/project-managers`} exact component={ProjectManagerPage}></Route>
+                            <Route path={`${path}/contractors`} exact component={ContractorsPage}></Route>
+                        </Switch>
+
+                    </div>
+                </nav>
             </div>
         )
     }
