99aeb9fc5cbed582edcdceea778fa618fd2d8745
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-07-01 17:26:37 +0300

    wrote some tests

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,7 +1,7 @@
 
+import { mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 
 import LoginForm from './LoginForm';
 
@@ -9,10 +9,20 @@ import LoginForm from './LoginForm';
 it('renders without crashing', () => {
     const div = document.createElement('div');
     // ReactDOM.render(<App />, div);
-    ReactDOM.render(
-
-        <LoginForm />,
-
-        div);
+    const element = (<LoginForm />)
+    ReactDOM.render(element, div);
     ReactDOM.unmountComponentAtNode(div);
+    const mounted = mount(element);
+    // debugger
+    // expect(wrapper.find('LoginForm')).to.have.lengthOf(1);
+    return Promise
+        .resolve(mounted)
+        .then(() => mounted.update())
+        .then(() => {
+            const el = mounted.find('button').first()
+            // console.log(mounted.debug());
+            // console.log(el);
+            expect(el.length).toEqual(1);
+            expect(mounted.text()).toContain("Login");
+        });
 });
